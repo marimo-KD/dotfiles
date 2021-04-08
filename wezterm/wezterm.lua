bd64dfd6fd61354c33372f37ebb4c65ed033822e
marimo-KD <marimo.for.others@gmail.com>
2021-04-08 23:08:08 +0900

    update

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+ local wezterm = require'wezterm';
+
+function font_with_fallback(name, params)
+  local names = {name, "Symbols Nerd Font", "Noto Color Emoji"}
+  return wezterm.font_with_fallback(names, params)
+end
+
+return {
+  font = font_with_fallback("Sarasa Term J"),
+  font_rules = {
+    {
+      italic = true,
+      font = font_with_fallback("Sarasa Term J", {italic = true}),
+    },
+    {
+      intensity = "Bold",
+      font = font_with_fallback("HackGenNerd Console", {bold = true}),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = font_with_fallback("Sarasa Term J", {bold = true, italic=true}),
+    },
+    {
+      intensity = "Half",
+      font = font_with_fallback("Sarasa Term J Light"),
+    },
+  },
+  font_size = 9.5,
+  font_antialias = "Greyscale",
+  font_hinting = "Full",
+  window_background_opacity = 0.95,
+  window_close_confirmation = "NeverPrompt",
+  enable_tab_bar = false,
+  enable_scroll_bar = false,
+  color_scheme = "Gruvbox Dark",
+  default_cursor_style = "BlinkingBar",
+}
