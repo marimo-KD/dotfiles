c52b9dad0df30225baf371eefc5611ef66bfa358
marimo-KD <marimo.for.others@gmail.com>
2024-07-03 06:32:25 +0900

    add zk-phi/setup.el

---
+++ home/editor/emacs/setup.nix
@@ -0,0 +1,13 @@
+{
+  trivialBuild,
+  fetchFromGitHub,
+}:trivialBuild rec {
+  pname = "setup.el";
+  version = "2022-04-14";
+  src = fetchFromGitHub {
+    owner = "zk-phi";
+    repo = "setup";
+    rev = "d5311e2ec32107f334e5030e3425f946633b5844";
+    hash = "";
+  };
+}
